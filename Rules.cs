0e0131ed7fc914aa4d368266559347f19d8eb0ff
witomin <witomin@mail.ru>
2023-02-02 14:35:04 +0300

    добавлен командлет Get-Rules

---
+++ Rules.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Management.Automation;
+using Yandex.API360.Models;
+
+namespace Y360Management {
+    /// <summary>
+    /// Получить правила обработки почты
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "Rules"), OutputType(typeof(List<Rule>))]
+    public class GetRulesCmdlet : PSCmdlet {
+        protected override void EndProcessing() {
+            var APIClient = Helpers.GetApiClient(this);
+            List<Rule> result = APIClient.GetRulesAsync().Result;
+            WriteObject(result);
+            base.EndProcessing();
+        }
+    }
+}
