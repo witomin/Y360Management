09ffe66c952a8d27f693a002303ac79d6c3e9df9
witomin <witomin@mail.ru>
2023-03-22 10:39:54 +0300

    Добавлен пример скрипта для выгрузки пользователей

---
+++ Examples of scripts/ExportUsersToCSV.ps1
@@ -0,0 +1,7 @@
+﻿# выгрузка списка пользователей Яндекс 360 в CSV файл
+$OrgID = "ID организации"
+$APIToken = "Токен доступа к API"
+$CSVPath = "Путь к CSV файлу"
+Import-Module Y360Management
+Connect-Y360 -OrgId $OrgID -APIToken $APIToken
+Get-Users | Select-object -Property id, nickname, departmentId,	email, name, gender, position, avatarId, about,	birthday,@{label="contacts"; expression={$_.contacts -Join ','}}, @{label="aliases"; expression={$_.aliases -Join ','}}, @{label="groups"; expression={$_.groups -Join ','}}, externalId, isAdmin, isRobot,	isDismissed, isEnabled,	timezone, language,	createdAt, updatedAt |	Export-Csv -Path $CSVPath
