a9d116b8c4b09e634df3d097cfd8d3d582ddc298
witomin <witomin@mail.ru>
2023-03-22 11:30:55 +0300

    Добавлены примеры скриптов выгрузки отделов, групп

---
+++ Examples of scripts/ExportDepartmentsToCSV.ps1
@@ -0,0 +1,7 @@
+﻿# выгрузка списка подразделений Яндекс 360 в CSV файл
+$OrgID = "ID организации"
+$APIToken = "Токен доступа к API"
+$CSVPath = "Путь к CSV файлу"
+Import-Module Y360Management
+Connect-Y360 -OrgId $OrgID -APIToken $APIToken
+Get-Departments| Select-object -Property id, email, name, @{label="aliases"; expression={$_.aliases -Join ','}}, membersCount, description, externalId, headId, label, parentId, createdAt |	Export-Csv -Path $CSVPath
---
+++ Examples of scripts/ExportGroupsToCSV copy.ps1
@@ -0,0 +1,7 @@
+﻿# выгрузка списка групп Яндекс 360 в CSV файл
+$OrgID = "ID организации"
+$APIToken = "Токен доступа к API"
+$CSVPath = "Путь к CSV файлу"
+Import-Module Y360Management
+Connect-Y360 -OrgId $OrgID -APIToken $APIToken
+Get-Groups| Select-object -Property id, type, membersCount, email, @{label="aliases"; expression={$_.aliases -Join ','}}, removed, authorId, @{label="memberOf"; expression={$_.memberOf -Join ','}}, description, name, externalId, @{label="members"; expression={$_.members -Join ','}}, @{label="adminIds"; expression={$_.adminIds -Join ','}}, createdAt |	Export-Csv -Path $CSVPath
